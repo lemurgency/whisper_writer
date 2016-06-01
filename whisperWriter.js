79fdba90e43e2071553dcca7ae53baa492a82088
Spenser Home <spenserhome@Spensers-MacBook-Pro.local>
2016-05-31 22:30:20 -0500

    square and triangle together

--- whisperWriter.js
+++ whisperWriter.js
@@ -1,80 +1,157 @@
-function fillInTriangle(startCoords, lengthOfSide){
-	var coords = randomizeStartCoords(startCoords);
-	ctx.moveTo(coords[0][0],coords[0][1]);
-	ctx.lineTo(coords[1][0],coords[1][1]);
-	ctx.lineTo(coords[2][0],coords[2][1]);
-	ctx.lineTo(coords[3][0],coords[3][1]);	
+(function(){
+	window.whisp = {};
+	var d = 20;
+	
+	function fillInTriangle(startCoords, lengthOfSide){
+		var coords = randomizeStartCoords(startCoords);
+		ctx.moveTo(coords[0][0],coords[0][1]);
+		ctx.lineTo(coords[1][0],coords[1][1]);
+		ctx.lineTo(coords[2][0],coords[2][1]);
+		ctx.lineTo(coords[3][0],coords[3][1]);	
 
-	for(var i =0; i < lengthOfSide; i++){
-		var coordsLength = coords.length;
-		var last = coords[coordsLength-1];
-		var twoLast = coords[coordsLength-2];
-		var threeLast = coords[coordsLength-3]; 
+		for(var i =0; i < lengthOfSide; i++){
+			var coordsLength = coords.length;
+			var last = coords[coordsLength-1];
+			var twoLast = coords[coordsLength-2];
+			var threeLast = coords[coordsLength-3]; 
 
-		var vector = [threeLast[0] - twoLast[0], threeLast[1] - twoLast[1]]; //threeLast to twoLast 
+			var vector = [threeLast[0] - twoLast[0], threeLast[1] - twoLast[1]]; //threeLast to twoLast 
 
-		var divisor = Math.sqrt(vector[0]*vector[0] + vector[1]*vector[1]);
-		var u = [vector[0]/divisor, vector[1]/divisor];
+			var divisor = Math.sqrt(vector[0]*vector[0] + vector[1]*vector[1]);
+			var u = [vector[0]/divisor, vector[1]/divisor];
 
-		var du = [u[0]*d, u[1]*d];
+			var du = [u[0]*d, u[1]*d];
 
-		var point = [threeLast[0] - du[0], threeLast[1] - du[1]]
+			var point = [threeLast[0] - du[0], threeLast[1] - du[1]]
 
 
-		coords.push(point)
-		ctx.lineTo(point[0], point[1])
+			coords.push(point)
+			ctx.lineTo(point[0], point[1])
+		}
+		ctx.stroke();
 	}
-	ctx.stroke();
-}
 
-function randomizeStartCoords(startCoords){
-	var slimmed = startCoords.slice(0,3);
+	function fillInTriangle2(startCoords, lengthOfSide){
+		// var coords = randomizeStartCoords(startCoords);
+		var coords = startCoords;
+		ctx.moveTo(coords[0][0],coords[0][1]);
+		ctx.lineTo(coords[1][0],coords[1][1]);
+		ctx.lineTo(coords[2][0],coords[2][1]);
+		ctx.lineTo(coords[3][0],coords[3][1]);	
+		ctx.lineTo(coords[4][0],coords[4][1]);	
 
-	var shuffled = shuffle(slimmed);
+		for(var i =0; i < lengthOfSide; i++){
+			var coordsLength = coords.length;
+			var last = coords[coordsLength-1];
+			var twoLast = coords[coordsLength-2];
+			var threeLast = coords[coordsLength-3]; 
+			var fourLast = coords[coordsLength-4]; 
 
-	shuffled.push(shuffled[0]);
+			var vector = [fourLast[0] - threeLast[0], fourLast[1] - threeLast[1]]; //fourLast to threeLast 
 
-	return shuffled;
+			var divisor = Math.sqrt(vector[0]*vector[0] + vector[1]*vector[1]);
+			var u = [vector[0]/divisor, vector[1]/divisor];
 
-}
+			var du = [u[0]*d, u[1]*d];
 
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+			var point = [fourLast[0] - du[0], fourLast[1] - du[1]]
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+			coords.push(point)
+			ctx.lineTo(point[0], point[1])
+		}
+		ctx.stroke();
+	}
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+	function randomizeStartCoords(startCoords){
+		var numberOfPoints = startCoords.length -1;
+		var slimmed = startCoords.slice(0,numberOfPoints);
 
-  return array;
-}
+		var shuffled = shuffle(slimmed);
 
-function buildMap(canvasSize, triSize){
-	for(var x = 0; x<canvasSize/triSize; x++){
-		for(var y = 0; y<canvasSize/triSize; y++){
-			var coords = [
+		shuffled.push(shuffled[0]);
+
+		return shuffled;
+
+	}
+
+	function shuffle(array) {
+	  var currentIndex = array.length, temporaryValue, randomIndex;
+
+	  // While there remain elements to shuffle...
+	  while (0 !== currentIndex) {
+
+	    // Pick a remaining element...
+	    randomIndex = Math.floor(Math.random() * currentIndex);
+	    currentIndex -= 1;
+
+	    // And swap it with the current element.
+	    temporaryValue = array[currentIndex];
+	    array[currentIndex] = array[randomIndex];
+	    array[randomIndex] = temporaryValue;
+	  }
+
+	  return array;
+	}
+
+	whisp.buildMap = function(canvasSize, triSize){
+		for(var x = 0; x<canvasSize/triSize; x++){
+			for(var y = 0; y<canvasSize/triSize; y++){
+				var coords = [
+						[x*triSize,y*triSize],
+						[(x+1)*triSize,(y+1)*triSize],
+						[x*triSize,(y+1)*triSize],
+						[x*triSize,y*triSize]
+					]
+				var coords2 = [
+						[(x+1)*triSize,y*triSize],
+						[(x+1)*triSize,(y+1)*triSize],
+						[x*triSize,y*triSize],
+						[(x+1)*triSize,y*triSize]
+					]
+
+				fillInTriangle(coords, triSize);
+				fillInTriangle(coords2, triSize);
+
+			}
+		}
+	}
+
+	whisp.buildMap2 = function(canvasSize, triSize){
+		for(var x = 0; x<canvasSize/triSize; x++){
+			for(var y = 0; y<canvasSize/triSize; y++){
+				var coords1 = [
 					[x*triSize,y*triSize],
 					[(x+1)*triSize,(y+1)*triSize],
 					[x*triSize,(y+1)*triSize],
 					[x*triSize,y*triSize]
 				]
-			var coords2 = [
+				var coords2 = [
 					[(x+1)*triSize,y*triSize],
 					[(x+1)*triSize,(y+1)*triSize],
 					[x*triSize,y*triSize],
 					[(x+1)*triSize,y*triSize]
 				]
+				var coords3 = [
+					[x*triSize,y*triSize],
+					[(x+1)*triSize,y*triSize],
+					[(x+1)*triSize,(y+1)*triSize],
+					[x*triSize,(y+1)*triSize],
+					[x*triSize,y*triSize]
+				]
 
-			fillInTriangle(coords, triSize);
-			fillInTriangle(coords2, triSize);
+				var random = Math.round(Math.random())
+				if(random == 1) {
+					fillInTriangle2(coords3, triSize)
+				} else {
+					fillInTriangle(coords1, triSize);
+					fillInTriangle(coords2, triSize);
+				}
+			}
 		}
 	}
-}
+	return whisp;
+})()
+
+
+
